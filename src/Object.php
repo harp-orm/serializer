f38f70486ab0ad682e1bfb51bb0dbed2391621a7
Ivan Kerin <ikerin@gmail.com>
2014-07-17 14:29:39 +0300

    Add Object Serializer

---
+++ src/Object.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Harp\Serializer;
+
+/**
+ * @author    Ivan Kerin <ikerin@gmail.com>
+ * @copyright (c) 2014 Clippings Ltd.
+ * @license   http://spdx.org/licenses/BSD-3-Clause
+ */
+class Object extends AbstractSerializer
+{
+    private $class;
+
+    /**
+     * @param string $name
+     */
+    public function __construct($name, $class)
+    {
+        parent::__construct($name);
+
+        $this->class = $class;
+    }
+
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+    public function newInstance()
+    {
+        $class = $this->class;
+
+        return new $class();
+    }
+
+    /**
+     * @param  object|array $subject
+     * @return mixed
+     */
+    public function serializeProperty($subject)
+    {
+        $object = $this->getProperty($subject);
+
+        return $object->serialize();
+    }
+
+    /**
+     * @param  object|array $subject
+     * @return mixed
+     */
+    public function unserializeProperty($subject)
+    {
+        $object = $this->newInstance();
+
+        $data = $this->getProperty($subject);
+
+        if ($data) {
+            $object->unserialize($data);
+        }
+
+        return $object;
+    }
+}
